8ce19c4104c48c823c48db47d4cb0bfc68acd9e0
alberto <albzamlan@alu.edu.gva.es>
2025-01-10 08:39:15 +0100

    InterfacesAdministrativo

---
+++ TransitPort/src/app/model/buque.ts
@@ -0,0 +1,7 @@
+export interface Buque {
+    id: number;
+    amarre: string;
+    procedencia: string;
+    destino: string;
+    id_administrativo: number;
+}
---
+++ TransitPort/src/app/model/turno.ts
@@ -0,0 +1,5 @@
+export interface Turno {
+    id: number;
+    fecha_inicio: Date;
+    fecha_fin: Date;
+}
