2574abb1a89753858d76371177150e07bda78e9c
julia <julia.g.avila@gmail.com>
2025-01-10 09:09:34 +0100

    solucionadoReal

--- TransitPort/src/app/gestor/gestor.ts
+++ TransitPort/src/app/gestor/gestor.ts
@@ -1,14 +1,11 @@
 export interface Gestor {
   id:number;
   nombre:string;
-<<<<<<< HEAD
   apellidos:string;
   email:string;
   password:string;
   telefono:string;
   dni:string;
-=======
   usuario:string;
   contrasena:string;
->>>>>>> origin
 }
--- TransitPort/src/app/gestor/grua.ts
+++ TransitPort/src/app/gestor/grua.ts
@@ -2,9 +2,6 @@ export interface Grua {
   id:number;
   capacidad_carga:number;
   estado:string;
-<<<<<<< HEAD
   id_getor:number;
-=======
   id_gestor:number;
->>>>>>> origin
 }
