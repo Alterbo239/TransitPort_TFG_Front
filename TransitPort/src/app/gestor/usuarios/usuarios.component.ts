1981ab1e3d2b6e07c985c7d2db2144b6d773126d
julia <julia.g.avila@gmail.com>
2025-01-23 11:48:21 +0100

    mal

--- TransitPort/src/app/gestor/usuarios/usuarios.component.ts
+++ TransitPort/src/app/gestor/usuarios/usuarios.component.ts
@@ -1,22 +1,15 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { DataTablesModule } from 'angular-datatables';
-import { Config } from 'datatables.net';
-import { Subject } from 'rxjs';
+
 
 @Component({
   selector: 'app-usuarios',
-  imports: [DataTablesModule],
+  imports: [],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css'
 })
 export class UsuariosComponent {
-  dtoptions: Config = {};
-  dtTrigger: Subject<any> = new Subject<any>();
 
-  ngOnInit(): void {
-    this.dtoptions = {
-      pagingType: 'full_numbers'
-    };
-  }
+
+
 }
