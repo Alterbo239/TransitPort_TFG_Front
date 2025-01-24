9b3d30118ab8cfc49cd89742e6c9adfbaad62062
alberto <albzamlan@alu.edu.gva.es>
2025-01-24 09:54:57 +0100

    MenuAdminMal

--- TransitPort/src/app/administrativo/administrativo.component.ts
+++ TransitPort/src/app/administrativo/administrativo.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-administrativo',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MenuComponent],
   templateUrl: './administrativo.component.html',
   styleUrl: './administrativo.component.css'
 })
