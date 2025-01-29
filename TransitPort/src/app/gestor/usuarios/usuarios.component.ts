40bc3a4ae1660f18c316c10b753a0a4e43c39584
alberto <albzamlan@alu.edu.gva.es>
2025-01-29 08:10:35 +0100

    cdkNoUsar

--- TransitPort/src/app/gestor/usuarios/usuarios.component.ts
+++ TransitPort/src/app/gestor/usuarios/usuarios.component.ts
@@ -1,21 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Config } from 'datatables.net';
 
 @Component({
   selector: 'app-usuarios',
-  imports: [],
+  imports: [ ],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css'
 })
-export class UsuariosComponent implements OnInit {
-
-  dtOptions: Config = {};
-
-  ngOnInit(): void {
-    this.dtOptions = {
-      pagingType: 'full_numbers'
-    };
-  }
-
-
+export class UsuariosComponent {
+  
 }
