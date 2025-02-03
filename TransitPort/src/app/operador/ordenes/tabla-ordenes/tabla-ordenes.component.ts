c0c98b0b558b66fd5898937c697a897f03e88473
Lucas Duarte <lduarte@iesfuentesanluis.com>
2025-02-03 09:16:01 +0100

    03-02-25 -- 02

--- TransitPort/src/app/operador/ordenes/tabla-ordenes/tabla-ordenes.component.ts
+++ TransitPort/src/app/operador/ordenes/tabla-ordenes/tabla-ordenes.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, Renderer2 } from '@angular/core';
 import { Subject } from 'rxjs';
 import { DataTablesModule } from 'angular-datatables';
 import { Router, RouterModule} from '@angular/router';
+import { SuppliersService } from '../../../data.service';
+import { Suppliers } from '../../../data.service';
+import { Config } from 'datatables.net';
 
 
 @Component({
@@ -11,9 +14,88 @@ import { Router, RouterModule} from '@angular/router';
   templateUrl: './tabla-ordenes.component.html',
   styleUrls: ['./tabla-ordenes.component.css']
 })
-export class TablaOrdenesComponent {
+export class TablaOrdenesComponent implements OnInit{
 
-  constructor(private router: Router){}
+  dtOptions: Config = {};
+
+  constructor(
+    private router: Router,
+    private suppliersService: SuppliersService,
+    private renderer: Renderer2){}
+
+    ngOnInit(): void {
+      this.dtOptions = {
+        ajax: (dataTablesParameters: any, callback) => {
+          this.suppliersService.getSuppliersList().subscribe(resp => {
+            callback({
+              data: resp
+            });
+          });
+        },
+
+        //selección de cantidad de datos a mostrar en la tabla
+      lengthMenu : [4, 8, 12, 16],
+
+      //cantidad máxima de datos que se muestran en la tabla
+      scrollY: '600px',
+      scrollCollapse: true,
+      paging: false,
+      autoWidth: false,
+      destroy: true,
+      retrieve: true,
+      scrollX: true,
+
+
+
+      //configuración de la tabla a español
+      language: {
+        search: 'Buscar:',
+        lengthMenu: 'Mostrar  _MENU_',
+        info: 'Mostrando _START_ a _END_ de _TOTAL_ usuarios',
+        paginate: {
+          first: 'Primero',
+          last: 'Último',
+          next: 'Siguiente',
+          previous: 'Anterior'
+        },
+        emptyTable: 'No hay datos disponibles en la tabla'
+      },
+
+
+
+      //tipos de columnas y sus nombres
+      columns: [
+        { title: 'Cargo', data: 'cargo' },
+        { title: 'Nombre empleado', data: 'name' },
+        { title: 'Estado', data: 'estado' },
+
+      ],
+      rowCallback: (row: Node, data: any, index: number) => {
+
+        const rowElement = row as HTMLElement;
+
+        //estilo de la tabla
+
+        // const actionCell = rowElement.querySelector('table');
+        // if (actionCell) {
+        //   actionCell.setAttribute(
+        //     'style',
+        //     'display: flex; justify-content: center; '
+        //   );
+        // }
+
+        const actionButton = rowElement.querySelector('.action-btn');
+        if (actionButton) {
+          this.renderer.listen(actionButton, 'click', () => {
+            console.log('Row data:', data);
+          });
+        }
+        return row;
+      }
+
+    };
+
+  }
 
   pulsarOrden(): void {
     this.router.navigate(['operador/orden']);
