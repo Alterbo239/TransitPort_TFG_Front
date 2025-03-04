e583736bd6eec4c80ade4627c9d0026bef23bc66
Alberto Zamora <spikejrgd@gmail.com>
2025-03-04 22:16:59 +0100

    Actualizar orden workea bien

--- src/app/administrativo/monitorizar-orden/monitorizar-orden.component.ts
+++ src/app/administrativo/monitorizar-orden/monitorizar-orden.component.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { Config } from 'datatables.net';
 import { SuppliersService } from '../../services/data.service';
 import { DataTablesModule  } from 'angular-datatables';
+import Swal from 'sweetalert2';
+import { response } from 'express';
+import { data } from 'jquery';
+import { error } from 'console';
 
 @Component({
   selector: 'app-monitorizar-orden',
@@ -49,15 +53,101 @@ export class MonitorizarOrdenComponent implements OnInit{
       rowCallback: (row: Node, data: any, index: number) => {
 
         const rowElement = row as HTMLElement;
-
-        const actionButton = rowElement.querySelector('.action-btn');
-        if (actionButton) {
-          this.renderer.listen(actionButton, 'click', () => {
-            console.log('Row data:', data);
+        
+        if (rowElement) {
+          this.renderer.listen(rowElement, 'click', () => {
+            this.mostrarAlerta(data);
           });
         }
         return row;
       }
     };
   }
-}
+
+  mostrarAlerta(data: any) {
+    Swal.fire({
+      title: 'Detalles',
+      html: `
+        <h3>Codigo de la orden</h3>
+        <input type="text" value="${data.id}" disabled>
+        <br>
+        <h3>Tipo de orden</h3>
+        <input type="text" value="${data.tipo}" disabled>
+
+        <h3>Cantidad contenedores</h3>
+        <input type="text" value="${data.cantidad_contenedores}" disabled>
+        <br>
+        <h3>Estado</h3>
+        <input class="${data.estado}" type="text" value="${data.estado}" disabled>
+      `,      
+      confirmButtonText: 'Actualizar',
+      cancelButtonText: 'Cerrar',
+
+      showCancelButton: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: 'Actualizar',
+          html: `
+            <h3>ID Grua</h3>
+            <input id="id_grua" style="border: solid 1px black" type="text" value="${data.id_grua}">
+            <br>
+            <h3>ID Buque</h3>
+            <input id="id_buque" style="border: solid 1px black" type="text" value="${data.id_buque}">
+            <br>
+            <h3>ID Zona</h3>
+            <input id="id_zona" style="border: solid 1px black" type="text" value="${data.id_zona}">
+            <br>
+            <h3>ID Operador</h3>
+            <input id="id_operador" style="border: solid 1px black" type="text" value="${data.id_operador}">
+          `,
+          preConfirm: () => {
+            let grua = (document.getElementById('id_grua') as HTMLInputElement).value;
+            let buque = (document.getElementById('id_buque') as HTMLInputElement).value;
+            let zona = (document.getElementById('id_zona') as HTMLInputElement).value;
+            let operador = (document.getElementById('id_operador') as HTMLInputElement).value;
+
+            return Promise.all([
+              this.suppliersService.validarGrua(grua).toPromise(),
+              this.suppliersService.validarBuque(buque).toPromise(),
+              this.suppliersService.validarZona(zona).toPromise(),
+              this.suppliersService.validarOperador(operador).toPromise()
+            ]).then(([gruaValida, buqueValido, zonaValida, operadorValido]) => {
+              console.log(gruaValida, buqueValido, zonaValida, operadorValido);
+              if (gruaValida && buqueValido && zonaValida && operadorValido) {
+                return { grua, buque, zona, operador };
+              } else {
+                Swal.showValidationMessage('Uno o más campos no son válidos, asegúrate de que los datos sean correctos');
+                return false;
+              }
+            });
+          }
+        }).then((result) => {
+          if (result.isConfirmed && result.value) {
+            let updatedData = {
+              id: data.id,
+              tipo: data.tipo,
+              cantidad_contenedores: data.cantidad_contenedores,
+              fecha_inicio: data.fecha_inicio,
+              fecha_fin: data.fecha,
+              estado: data.estado,
+              id_administrativo: data.id_administrativo,
+              id_grua: result.value.grua,
+              id_buque: result.value.buque,
+              id_zona: result.value.zona,
+              id_operador: result.value.operador,
+            };
+            this.suppliersService.actualizarOrdenes(updatedData).subscribe(
+              (response) => {
+                Swal.fire('Exito', 'Orden actualizada correctamente', 'success')
+                .then(() => {
+                  window.location.reload();
+                });
+              }
+            )
+          }
+        })
+      }
+    });
+    }
+  }
--- src/app/services/orden.service.ts
+++ src/app/services/orden.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Orden } from "../models/orden";
 import { Incidencia } from '../models/incidencia';
+import { map } from 'rxjs/operators';
+import { Orden2 } from '../models/orden2';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +22,36 @@ export class OrdenService {
     return this.http.get<any[]>(`http://localhost:8000/api/operador/ordenes?id_operador=${id}`);
 
   }
+
+  actualizarOrdenes(orden: Orden2) {
+    return this.http.put<Orden2>(`http://localhost:8000/api/orden/actualizar/${orden.id}`, orden);
+  }
+
+  validarGrua(id: any): Observable<boolean> {
+    return this.http.get(`http://localhost:8000/api/grua/show/${id}`).pipe(
+      map(() => true),
+      catchError(() => of(false))
+    );
+  }
+  validarBuque(id: any): Observable<boolean> {
+    return this.http.get(`http://localhost:8000/api/buque/show/${id}`).pipe(
+      map(() => true),
+      catchError(() => of(false))
+    );
+  }
+  validarZona(id: any): Observable<boolean> {
+    return this.http.get(`http://localhost:8000/api/zona/show/${id}`).pipe(
+      map(() => true),
+      catchError(() => of(false))
+    );
+  }
+  validarOperador(id: any): Observable<boolean> {
+    return this.http.get(`http://localhost:8000/api/operador/show/${id}`).pipe(
+      map(() => true),
+      catchError(() => of(false))
+    );
+  }
+
   actualizarEstado(orden: Orden): Observable<Orden> {
     return this.http.put<Orden>(`http://localhost:8000/api/operador/ordenes/orden/${orden.id}`, orden);
   }
